c4b9672f22a6a0886e511a816ff4578da84ba3fb
bjma <bjma@ucsc.edu>
2020-12-26 01:26:20 -0800

    set API query response to a limit of 50 items

--- controllers/proj1/imageController.js
+++ controllers/proj1/imageController.js
@@ -5,19 +5,19 @@
 const Image = require('../../models/proj1/imageModel');
 
  // GET (read) endpoint
- exports.index = async (req, res) => {
+exports.index = async (req, res) => {
     // Really long string of functions that essentially converts the Mongoose document to JSON
     // Source: https://stackoverflow.com/questions/9952649/convert-mongoose-docs-to-json
-    let data = await Image.findOne().lean().exec((err, images) => {
-        return res.end(JSON.stringify(images));
+    let data = await Image.find({}, null, {limit: 50}).lean().exec((err, content) => {
+        return res.end(JSON.stringify(content));
     });
  }
 
 // DELETE (remove) endpoint
- exports.delete = async (req, res) => {
+exports.delete = async (req, res) => {
      await Image.remove({
          _id: req.params._id
-     }, (err, image) => console.log(err));
+     }, (err, content) => console.log(err));
      res.json({
          status: "success",
          message: "Successfully deleted image."
