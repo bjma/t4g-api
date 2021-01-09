b6750e314e68e2fd6c78a2f22f0365c7dcb7243c
bjma <bjma@ucsc.edu>
2021-01-08 16:51:50 -0800

    Set the request handlers to actually return a response/

--- controllers/proj1/imageController.js
+++ controllers/proj1/imageController.js
@@ -1,5 +1,6 @@
 /**
  * fileName: imageController.js
+ * 
  * Controller for image data; handles requests and defines endpoints
  */
 const Image = require('../../models/proj1/imageModel');
@@ -20,15 +21,15 @@ exports.index = async (req, res) => {
 // POST endpoint; adds a new image to the database (might tweak it to add all images)
 exports.new = async (req, res) => {
     let image = new Image();
-    image.name = req.body.name ? req.body.name : contact.name;
+    image.name = req.body.name ? req.body.name : image.name;
     image.byteData = req.body.byteData;
     image.features = req.body.features;
     // Save image
     image.save((err) => {
         if (err) {
-            res.json(err);
+            return res.json(err);
         } else {
-            res.json({
+            return res.json({
                 message: "Successfully added new image.",
                 data: image,
             });
