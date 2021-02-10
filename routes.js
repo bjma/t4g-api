197817550cd177fda61fee1a24264d5bed6efbfe
bjma <bjma@ucsc.edu>
2021-02-10 15:07:13 -0800

    Cleaned router code

--- routes.js
+++ routes.js
@@ -5,11 +5,17 @@
  * Each route support their listed methods.
  */
 
- /* Modules */
+ /* Dependencies */
 const express = require('express');
 const router = express.Router();
 
-/* Default API response */
+/* Exported modules */
+const textController = require('./controllers/proj2/textController');
+const textControllerDemo = require('./controllers/demos/textController.demo');
+
+/** 
+ * Default API response
+ */
 router.get('/', (req, res) => {
     res.json({
         status: "OK",
@@ -17,41 +23,45 @@ router.get('/', (req, res) => {
     })
 });
 
-/* Routes to image datasets for Project 1 */
-const imageController = require('./controllers/proj1/imageController');
+/**
+ * Routes for datasets 
+ */ 
 router.route('/datasets/proj1')
     .get((req, res) => {
         res.json({
             message: "This endpoint is deprecated."
         });
     });
-
-/* Routes to text dataset for Project 2 */ 
-const textController = require('./controllers/proj2/textController');
 router.route('/datasets/proj2')
     .get(textController.index)
     .post(textController.new);
 
-/* Routes for API functionality demo */
-const textControllerDemo = require('./controllers/demos/textController.demo');
-router.route('/demos/nlp')
-    .get(textControllerDemo.index)
-    .post(textControllerDemo.new);
-
-/* Routes for the frontend interface used to upload scraped data.
-    This is specifically for Project 2. */
+/** 
+ * Routes for the frontend interface used to upload scraped data.
+ * This is specifically for Project 2.
+ */ 
 router.route('/post/proj2')
     .get((req, res) => {
         res.render('text-post');
     });
 
-/* Routes for error handling */
-router.get('/errors', (req, res) => {
-    res.json({
-        status: "ERROR",
-        message: "Oops! Something went wrong, probably a bad request."
+/**
+ * Routes for error handling 
+ */
+router.route('/errors')
+    .get((req, res) => {
+        res.json({
+            status: "ERROR",
+            message: "Oops! Something went wrong, probably a bad request."
+        })
     });
-});
+
+/**
+ * Routes for API functionality demo 
+ */
+router.route('/demos/nlp')
+    .get(textControllerDemo.index)
+    .post(textControllerDemo.new);
 
 /* Export routes to other files */
 module.exports = router;
